00ba7187bfe9ca67da5f21c793340af5ec1cdd0c
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-07-29 19:29:02 +0700

    demo event

---
+++ js/demo3.js
@@ -0,0 +1,31 @@
+function f1(){
+    alert("Function 1....");
+}
+
+function f2(){
+    alert("Function 2.....");
+}
+
+function f3(){
+    alert("Function 3.....");
+}
+
+var ip = document.getElementById("ip");
+var ts = document.getElementById("text-search");
+var ts2 = document.getElementById("text-search2");
+
+function key1(){
+    alert("Funtion key 1....");
+}
+function key2(){
+    var v = ip.value;
+    ts.innerText = v;
+}
+function key3(){
+    var v = ip.value;
+    ts2.innerText = v;
+}
+
+function key4(){
+    alert("Key 4....");
+}
